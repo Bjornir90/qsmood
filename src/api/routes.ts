bc7055b59dbe4b9bf666e59c1a20440e86e81469
Bjornir90 <celestincollin@gmail.com>
2020-10-09 16:01:14 +0200

    Fixes regex for date matching

--- src/api/routes.ts
+++ src/api/routes.ts
@@ -6,4 +6,17 @@ router.get("/test", (req, res) => {
     res.send("<h1>Test succesful</h1>");
 })
 
+router.get("/day/:date", (req, res) => {
+    let date = req.params.date;
+    let regex = /^\d{4}-\d{2}-\d{2}$/;
+    if(regex.exec(date) === null){
+        res.setHeader('Content-Type', 'application/json');
+        res.status(400).json({"error": "Date is not properly formatted"});
+        return;
+    }
+
+    //TODO retrieve document in DB
+    res.sendStatus(200);
+})
+
 export default router;
