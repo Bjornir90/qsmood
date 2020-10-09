9a4970d5ecd016300f14db4cbcbdbdb64d794461
Bjornir90 <celestincollin@gmail.com>
2020-10-09 21:31:30 +0200

    Migrate from vue-material to vuetify

---
+++ src/views/DatabaseDashboard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-container>
+
+    <v-card>
+
+    <v-list class="mx-auto" tile>
+
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Item de la liste</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title>Item de la liste 2</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+    </v-list>
+
+    </v-card>
+
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  
+})
+</script>
